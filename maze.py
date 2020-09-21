10384cd8295200200958d0840a901a0f1015923b
salmo0135 <lukesalmon13@gmail.com>
2020-09-20 17:03:45 -0700

    first upload

---
+++ maze.py
@@ -0,0 +1,25 @@
+"""
+Group assignment
+Taymoor
+Ijaz
+Luke
+
+"""
+
+
+class Maze:
+    def __init__(self,filename):
+        self.filename = filename
+        pass
+
+    def check(self,lnum,cnum):
+        pass
+
+    def display(self):
+        pass
+
+    def find_random_spot(self):
+        pass
+
+
+
