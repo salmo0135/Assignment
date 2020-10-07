6949cff163cd9ce78c60fa65e48c09e5130230e2
ijaz-hussain <62718643+ijaz-hussain@users.noreply.github.com>
2020-10-07 10:48:51 -0700

    Update maze.py
    Added is_item method and updated documentation

--- maze.py
+++ maze.py
@@ -1,49 +1,52 @@
 """
-Group assignment
+maze.py - Group assignment
 Taymoor
 Ijaz
 Luke
-
 """
 
 from random import randrange
-STARTING_POINT_OF_PLAYER = 'p' ;
+STARTING_POINT_OF_PLAYER = 'p'
 
 
 class Maze:
-    
     """ 
-    Nested list created from file representing maze
-    :param filename: file with maze on it
+    Nested list created from text file representing a maze
+
+    :param filename: text file containing maze
     :type filename: string
-    
-    :param _grid: nested list representing maze coordinates
-    :type _grid: list
-    
     """
     
-    def __init__(self, _filename, _grid):
+    def __init__(self, _filename):
+        """
+        Initialization of instance attributes
+        """
+        # -- open text file containing maze
         self.file = open(_filename, 'r')
         self._grid = []
+        # -- initialize line_list and append into list
         line_list = []
         lines = self.file.readlines()
         for line in lines:
             line = line.strip('\n')
             line_list = [char for char in line]
             self._grid.append(line_list)
-         #-- placing the player at the very start
-        _grid[0][0] = STARTING_POINT_OF_PLAYER;
+         # -- placing the player at the very start
+        self._grid[6][0] = STARTING_POINT_OF_PLAYER
+        
+        
 
-    #--- Rename the check method to can_move_to
-    """
-        if the location is a wall return False
-        otherwise return True
-    """
+    # --- Rename the check method to can_move_to
+        """ 
+        :return: return False if the location is a wall, otherwise return True
+        :rtype: bool
+        """
 
     #--@classmethod doesnt need to be a class method likewise for every other class method
     def can_move_to(self, line_num, col_num):
         """
         Check if coordinate is an empty space or wall
+
         :param line_num: establishes line number in grid
         :type line_num: integer
         
@@ -52,14 +55,13 @@ class Maze:
         
         :return: False if coordinates are a wall, True if empty space
         :rtype: bool
-        
         """
+        # -- checks if the coordinate is an empty space
         if self._grid[line_num][col_num] == "X":
             return False
         else:
             return True
         
-    @classmethod
     def display(self):
         """
         Prints the grid on the screen
@@ -67,7 +69,7 @@ class Maze:
         for line in self._grid:
             print("".join(line))
 
-    @classmethod        
+
     def find_random_spot(self):
         """
         Finds a random open spot in the maze
@@ -78,37 +80,57 @@ class Maze:
         random_line_index = []
         random_col = 0
         random_col_index = 100
-        
+
+        # -- loops through maze and stops when it finds an open spot
         while random_col != " ":
             random_line_index = randrange(len(self._grid))
             random_col_index = randrange(len(self._grid[random_line_index]))
             random_col = self._grid[random_line_index][random_col_index]
-        
+        # -- returns the coordinates of the open spot
         coordinates = (random_line_index, random_col_index)
         return coordinates
 
+    @property
+    def grid(self):
+        return self._grid
 
     #--add random items
-    """
+    
+    @grid.setter
+    def grid(self, value):
+        self._grid = value
+    
+    def add_items(self):
+        """
+        Adds random items to the maze
+        """
+        # -- item list
         item = ['S','H','R','M']
-        for i in item
-            coordinates = find_random_spot()
-            _grid(coordinates) = i
-    """
+        # -- loops through item list and adds them to the maze
+        for i in item:
+            x_coordinate, y_coordinate = self.find_random_spot()
+            self.grid[x_coordinate][y_coordinate] = i
+            print(x_coordinate, y_coordinate)
+        print(self._grid)
 
-    """
-    def is_item(self):
-        if self.grind[row][col] == randomItem:
-            return True
+
+   
+    def is_item(self, x_coordinate, y_coordinate):
+        """
+        If the location requested is a random item, return True
+
+        :param x_coordinate: x coordinate of the location requested
+        :type x_coordinate: integer
+
+        :param y_coordinate: y coordinate of the location requested
+        :type y_coordinate: integer
+
+        :return: return False if the location requested is not a random item, else return True
+        :rtype: bool
+        """
+        if self.grid[x_coordinate][y_coordinate] == "X" or self.grid[x_coordinate][y_coordinate] == " ":
+            return False
         else:
-            return False   
+            return True
         
-     """
-    """
     
-    """
-
-    
-            
-
-
