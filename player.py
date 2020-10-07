3f9fc351fe5da0bfbdb7d58421a9927f79fd56ad
ijaz-hussain <62718643+ijaz-hussain@users.noreply.github.com>
2020-10-07 10:46:56 -0700

    Update player.py
    Added documentation to player.py

--- player.py
+++ player.py
@@ -1,9 +1,23 @@
+"""
+player.py - Group assignment
+Taymoor
+Ijaz
+Luke
+"""
 
 backpack = []
 
 class Player:
+    """
+    Contains an attribute named backpack that will store the random items picked up
 
+    :param_ _backpack: Object used to store random items
+    :rtype: object
+    """
 
     def __init__(self,_backpack):
+        """
+        Initialization of instance attributes
+        """
         self._backpack = []
         pass
