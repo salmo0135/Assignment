4d5bde3347e8dc80162703c44880d00602009c46
Taymoor <61752890+taymoorkhan@users.noreply.github.com>
2020-10-09 02:43:54 -0700

    Update main.py
    Uploaded wrong file, lol

--- main.py
+++ main.py
@@ -1,158 +1,69 @@
-"""
-maze.py - Group assignment
-Taymoor
-Ijaz
-Luke
-"""
-
-from random import randrange
+# -- Main Function -- #
+from maze import Maze
 from player import Player
-POINT_OF_PLAYER = 'P'
-POINT_OF_EXIT = 'E'
+
+def main():
+    maze = Maze("grid01.txt")
+    end = False
+    maze.add_items()
+    for line in maze.grid:
+            print(''.join(line))
+    cmd = input()
+    while cmd != "q" and end == False:
+        cmd = input()
+        x, y = maze.player_location()
 
 
-class Maze:
-    """ 
-    Nested list created from text file representing a maze
-    :param filename: text file containing maze
-    :type filename: string
-    """
-    
-    def __init__(self, _filename):
-        """
-        Initialization of instance attributes
-        """
-        # -- open text file containing maze
-        self.file = open(_filename, 'r')
-        self._grid = []
-        # -- initialize line_list and append into list
-        line_list = []
-        lines = self.file.readlines()
-        for line in lines:
-            line = line.strip('\n')
-            line_list = [char for char in line]
-            self._grid.append(line_list)
-         # -- placing the player at the very start
-        self._player = Player(6,1)
-        self._grid[self._player._x][self._player._y] = POINT_OF_PLAYER
-        self._grid[1][-1] = POINT_OF_EXIT
-        
+        if cmd == "a" or cmd == "A":
+            
+            if maze._grid[x][y-1] != "X":
+                if maze._grid[x][y-1] != " ":
+                    maze._player._backpack.append(maze._grid[x][y-1])
+                maze._grid[x][y] = " "
+                y -= 1
+                maze._grid[x][y] = "P"
+                
         
 
-    # --- Rename the check method to can_move_to
-        """ 
-        :return: return False if the location is a wall, otherwise return True
-        :rtype: bool
-        """
+        elif cmd == "s" or cmd == "S":
 
-    #--@classmethod doesnt need to be a class method likewise for every other class method
-    def can_move_to(self, line_num, col_num):
-        """
-        Check if coordinate is an empty space or wall
-        :param line_num: establishes line number in grid
-        :type line_num: integer
-        
-        :param col_num: establishes column number in grid
-        :type col_num: integer
-        
-        :return: False if coordinates are a wall, True if empty space
-        :rtype: bool
-        """
-        # -- checks if the coordinate is an empty space
-        if self._grid[line_num][col_num] == " ":
-            return True
-        else:
-            return False
-        
-    def display(self):
-        """
-        Prints the grid on the screen
-        """
-        for line in self._grid:
-            print("".join(line))
+            if maze._grid[x+1][y] != "X":
+                if maze._grid[x+1][y] != " ":
+                    maze._player._backpack.append(maze._grid[x+1][y])
+                maze._grid[x][y] = " "
+                x += 1
+                maze._grid[x][y] = "P"
 
 
-    def find_random_spot(self):
-        """
-        Finds a random open spot in the maze
-        
-        :return: coordinates for open spot
-        :rtype: tuple
-        """
-        random_line_index = []
-        random_col = 0
-        random_col_index = 100
+        elif cmd == "w" or cmd == "W":
 
-        # -- loops through maze and stops when it finds an open spot
-        while random_col != " ":
-            random_line_index = randrange(len(self._grid))
-            random_col_index = randrange(len(self._grid[random_line_index]))
-            random_col = self._grid[random_line_index][random_col_index]
-        # -- returns the coordinates of the open spot
-        coordinates = (random_line_index, random_col_index)
-        return coordinates
+            if maze._grid[x-1][y] != "X":
+                if maze._grid[x-1][y] != " ":
+                    maze._player._backpack.append(maze._grid[x-1][y])
+                maze._grid[x][y] = " "
+                x -= 1
+                maze._grid[x][y] = "P"
+                
 
-    @property
-    def grid(self):
-        return self._grid
+        elif cmd == "d" or cmd == "D":
+            if maze._grid[x][y+1] != "X":
 
-    #--add random items
-    
-    @grid.setter
-    def grid(self, value):
-        self._grid = value
-    
-    def add_items(self):
-        """
-        Adds random items to the maze
-        """
-        # -- item list
-        item = ['S','H','R','M']
-        # -- loops through item list and adds them to the maze
-        for i in item:
-            x_coordinate, y_coordinate = self.find_random_spot()
-            self.grid[x_coordinate][y_coordinate] = i
+                if maze._grid[x][y+1] != " " and maze._grid[x][y+1] != "E":
+                    maze._player._backpack.append(maze._grid[x][y+1])
 
+                
+                y += 1
+                if maze._grid[x][y] != "E":
+                    maze.grid[x][y-1] = " "
+                    maze.grid[x][y] = "P"
+                else:
+                    if len(maze._player._backpack) == 4:
+                        print("Congratulations, You finished the game!")
+                        end = maze.is_exit(x, y)
+                
+        for line in maze.grid:
+            print(''.join(line))
+        print(maze._player._backpack)
 
+main()
 
-   
-    def is_item(self, x_coordinate, y_coordinate):
-        """
-        If the location requested is a random item, return True
-        :param x_coordinate: x coordinate of the location requested
-        :type x_coordinate: integer
-        :param y_coordinate: y coordinate of the location requested
-        :type y_coordinate: integer
-        :return: return False if the location requested is not a random item, else return True
-        :rtype: bool
-        """
-        if self.grid[x_coordinate][y_coordinate] == "X" or self.grid[x_coordinate][y_coordinate] == " " or self.grid[x_coordinate][y_coordinate] == POINT_OF_PLAYER or self.grid[x_coordinate][y_coordinate] == POINT_OF_EXIT:
-            return False
-
-        else:
-            return True
-    
-    def player_location(self):
-        """
-        Tracks player locations, returns x and y coordinate
-        """
-        x = 0
-        y = 0
-        for line in self.grid:
-            for i in line:
-                if i == "P":
-                    return x, y
-                    
-                y+=1
-            x += 1
-            y = 0
-
-    
-    def is_exit(self, x_coordinate, y_coordinate):
-        """
-        If the location requested is the exit, return True
-        """
-        if self.grid[x_coordinate][y_coordinate] == POINT_OF_EXIT:
-            return True
-        else:
-            return False
