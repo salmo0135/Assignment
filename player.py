b96b088d59b2688e22c3cceb292d68ba6188d778
Taymoor <61752890+taymoorkhan@users.noreply.github.com>
2020-10-08 21:48:19 -0700

    Update to Player class
    Added x and y coordinate attributes

--- player.py
+++ player.py
@@ -10,14 +10,15 @@ backpack = []
 class Player:
     """
     Contains an attribute named backpack that will store the random items picked up
-
     :param_ _backpack: Object used to store random items
     :rtype: object
     """
 
-    def __init__(self,_backpack):
+    def __init__(self, x_coordinate, y_coordinate):
         """
         Initialization of instance attributes
         """
         self._backpack = []
+        self._x = x_coordinate
+        self._y = y_coordinate
         pass
