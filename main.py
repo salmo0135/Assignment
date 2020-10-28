da64937970f98c634b73612f877e49a5d2d961ee
ijaz-hussain <62718643+ijaz-hussain@users.noreply.github.com>
2020-10-27 21:41:34 -0700

    Update main.py documentation

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-# -- Main Function -- #
+"""
+main.py - Group assignment
+Taymoor
+Ijaz
+Luke
+"""
+
+
 from maze import Maze
 from player import Player
 
+
 def main():
+    """
+    Program is executed in the main()
+    """
+    # -- Maze is displayed
     maze = Maze("grid01.txt")
     end = False
     maze.add_items()
@@ -13,7 +25,7 @@ def main():
         cmd = input()
         x, y = maze.player_location()
 
-
+        # -- if 'a' or 'A' key is pressed - move left
         if cmd == "a" or cmd == "A":
             
             if maze._grid[x][y-1] != "X":
@@ -24,7 +36,7 @@ def main():
                 maze.grid[x][y] = "P"
                 
         
-
+        # -- if 's' or 'S' key is pressed - move down
         elif cmd == "s" or cmd == "S":
 
             if maze.grid[x+1][y] != "X":
@@ -36,7 +48,7 @@ def main():
                 x += 1
                 maze.grid[x][y] = "P"
 
-
+        # -- if 'w' or 'W' key is pressed - move up
         elif cmd == "w" or cmd == "W":
 
             if maze.grid[x-1][y] != "X":
@@ -46,7 +58,7 @@ def main():
                 x -= 1
                 maze.grid[x][y] = "P"
                 
-
+        # -- if 'd' or 'D' key is pressed - move right
         elif cmd == "d" or cmd == "D":
             if maze.grid[x][y+1] != "X":
 
